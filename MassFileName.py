4d59bd54f6956ad573710ad4cedcd465fde94f04
YARandomPerson001 <jacobaharmon2004@gmail.com>
2024-04-07 21:43:05 -0500

    Addded command list

--- MassFileName.py
+++ MassFileName.py
@@ -1,3 +1,10 @@
 
+import os, sys
+
+CommandList = [ ['-d', '-dir', '--d', '--dir'],
+                ['-p', '-part', '--p', '--part'],
+                ['-r', '-replace', '--r', '--replace'],
+                ['-h', '-help', '--h', '--help'] ]
+
 if __name__ == "__main__":
     pass
