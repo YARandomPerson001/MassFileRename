a6f96d33fdf161f8a7aa1871cf49fbaa5abd7a31
YARandomPerson001 <jacobaharmon2004@gmail.com>
2024-04-07 22:13:13 -0500

    Loop though command and remove help

--- MassFileName.py
+++ MassFileName.py
@@ -1,10 +1,29 @@
 
 import os, sys
 
+Arguments = [ ]
+
 CommandList = [ ['-d', '-dir', '--d', '--dir'],
                 ['-p', '-part', '--p', '--part'],
                 ['-r', '-replace', '--r', '--replace'],
                 ['-h', '-help', '--h', '--help'] ]
 
+CommandDescription = [ "The path the mass rename!",
+                       "The word to replace.",
+                       "The word to replace with."
+                       "The command list!" ]
+
 if __name__ == "__main__":
+    # Loop though command line arguments.
+    for index in range(1, len(sys.argv)):
+        Flag = False
+        
+        for cmd in CommandList:
+            if sys.argv[index] == cmd:
+                
+                ## Todo: Print Command List
+                
+                Flag = True
+        
+        if not Flag: Arguments.append(sys.argv[index])
     pass
