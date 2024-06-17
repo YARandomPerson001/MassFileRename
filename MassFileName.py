acdd3ce1580e0526149b68d82eecc603c2b6d4c7
YARandomPerson001 <jacobaharmon2004@gmail.com>
2024-06-16 21:31:59 -0500

    Get a list

--- MassFileName.py
+++ MassFileName.py
@@ -1,12 +1,39 @@
 
 import os, sys, os.path
 
-Arguments = [ ]
+def GetFileName(Directory:str):
+    arr = os.listdir(Directory)
+    ret_arr:list = [ ]
+    
+    for f in arr:
+        ftemp = ''
+        etemp = ''
 
-Directory = "./"
-SelectedWord = ""
-ReplacmentWord = ""
+        flag = False
+
+        # Get Name
+        for i in range(0, len(f)):
+            if i > 0:
+                if f[i] == '.':
+                    break
+                else:
+                    ftemp += str(f[i])
+            else:
+                ftemp = str(f[i])
+
+        # Get extension
+        for i in range(0, len(f)):
+            if flag:
+                etemp += str(f[i])
+
+            if i > 0:
+                if f[i] == '.':
+                    flag = True
+        
+        flag = False
+        ret_arr.append((ftemp, etemp))
+
+    return ret_arr
 
 if __name__ == "__main__":
     pass
-    
